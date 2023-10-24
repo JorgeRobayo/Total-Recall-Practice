4a0b18d126d61c98141445695f60f2d5bbacdc7c
jorgerobayo <jorgerobmor@gmail.com>
2023-10-24 11:21:52 -0400

    question C

--- script.js
+++ script.js
@@ -66,3 +66,28 @@ let yourName = "Jorge"
 
 let newString = "Hello, my name is:" + " " + yourName
 console.log(newString)
+
+//=============================================================================
+
+//Using the provided variable definitions, replace the blanks so that all log statements print truein the console.
+//Answers should be all be valid JS syntax and not weird things 
+//that don't make sense but happen to print trueto the console
+
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+console.log(a < b);
+console.log(c > d);
+console.log('Name' ==='Name');
+// FOR THE NEXT TWO, USE ONLY && OR ||
+console.log(true || false);
+console.log(false || false || false || false || false || true);
+console.log(!false || false)
+console.log(e === 'Kevin');
+console.log(a + b == c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+console.log(a * a == d); // note: the answer is a simple arithmetic equation, not something "weird"
+console.log(48 == '48');
+
