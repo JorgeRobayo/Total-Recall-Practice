c183444c75bb75efb08ee1b138e7514f98fbf6ac
jorgerobayo <jorgerobmor@gmail.com>
2023-10-24 10:44:30 -0400

    Completed question A

---
+++ script.js
@@ -0,0 +1,32 @@
+//1. How do we assign a value to a variable?
+
+let color = "red"
+
+//2. How do we change the value of a variable?
+
+color = "blue"
+
+//3. How do we assign an existing variable to a new variable?
+
+let newColor = color
+
+//4. Remind me, what are declare, assign, and define?
+
+//Declare: we create a variable by using the word "let or const"
+//Assign: giving value after declaring a variable
+//Define: giving a variable its initial data type
+
+//5. What is pseudocoding and why should you do it?
+
+//What is: Pseudocoding is a way to write a description of a computer program or  algorithm in plain english
+//Why: We should use to simplify and planning the program in small logical steps. 
+
+
+//6. What percentage of time should be spent
+//thinking about how you're going to solve a problem vs actually typing in code to solve it?
+
+//A reasonable amount spent in planning and solving the problem is 50-70%
+//and for coding it takes about 50-30%
+
+//=============================================================================
+
